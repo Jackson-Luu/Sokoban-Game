1096f1f6585d5c1e3f0ba175f3d175bb072f3d24
kellerHuangCS <keller.huang98@gmail.com>
2017-05-09 19:45:56 +1000

    Update GameEngine.java

--- Project/src/GameEngine.java
+++ Project/src/GameEngine.java
@@ -5,5 +5,29 @@
  */
 
 public class GameEngine {
+    GameState currGame;
+    //move currently takes in a number between 1-4 corresponding to up, right, down, and left respectively
+    public GameEngine(int gameSizex, int gameSizey){
+        currGame = new GameState(new Map(generateMap(gameSizex,gameSizey)) , 0 , 0);
 
+    }
+
+    private int[][] generateMap(int x, int y){
+        int[][] map = new int[x][y];
+        //generate a map here
+        return map;
+    }
+
+    private Boolean checkValid(int move){
+        //if tile is empty
+        ArrayList<Integer> playerLoc;
+        if(move == 1){
+            if(currGame.getCurrMap().checkTile())
+        }
+        return true;
+    }
+
+    public void makeMove(int move){
+
+    }
 }
