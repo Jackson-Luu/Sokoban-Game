26766c30bf5a428f09ec3c3d5a0570521930d4c7
kellerHuangCS <keller.huang98@gmail.com>
2017-05-09 18:24:17 +1000

    Update GameState.java

--- Project/src/GameState.java
+++ Project/src/GameState.java
@@ -5,5 +5,28 @@
  */
 
 public class GameState {
+    private int moves;
+    //Time can be set to a different type later
+    private int time;
+    //Hints if we have a solve working
+    //private int Hints;
+    Map currMap;
 
+    public GameState(Map currMap, int currMoves, int time){
+        this.currMap = currMap;
+        moves = currMoves;
+        this.time = time;
+    }
+
+    public int getMoves(){
+        return moves;
+    }
+
+    public int getTime(){
+        return time;
+    }
+
+    public Map getCurrMap(){
+        return currMap;
+    }
 }
