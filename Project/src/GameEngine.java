638fbd44b2116d89b86e4b5d20e0a302f56fa8b2
kellerHuangCS <keller.huang98@gmail.com>
2017-05-10 22:01:41 +1000

    Update GameEngine.java

--- Project/src/GameEngine.java
+++ Project/src/GameEngine.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 /**
  * 
  * @author Jackson Luu, Keller Huang, Dan Yoo, Evan Han and Lilac Liu
@@ -8,7 +9,7 @@ public class GameEngine {
     GameState currGame;
     //move currently takes in a number between 1-4 corresponding to up, right, down, and left respectively
     public GameEngine(int gameSizex, int gameSizey){
-        currGame = new GameState(new Map(generateMap(gameSizex,gameSizey)) , 0 , 0);
+        currGame = new GameState(new Map(generateMap(gameSizex,gameSizey),gameSizex,gameSizey) , 0 , 0);
 
     }
 
@@ -20,14 +21,45 @@ public class GameEngine {
 
     private Boolean checkValid(int move){
         //if tile is empty
-        ArrayList<Integer> playerLoc;
+        //checks if tile is empty or occupied
+        //if tile is occupied by a box, checks the tile after
+        ArrayList<Integer> playerLoc = currGame.getCurrMap().checkPlayerLocation();
+        //move up
         if(move == 1){
-            if(currGame.getCurrMap().checkTile())
+            //moving off map
+            if(playerLoc.get(1) == 0) return false;
+            //checking tile above
+            if(currGame.getCurrMap().checkTile(playerLoc.get(0),playerLoc.get(1) - 1) == 0 || currGame.getCurrMap().checkTile(playerLoc.get(0),playerLoc.get(1) - 1) == 7){
+                return true;
+            }else if(currGame.getCurrMap().checkTile(playerLoc.get(0),playerLoc.get(1) - 1) == 5 || currGame.getCurrMap().checkTile(playerLoc.get(0),playerLoc.get(1) - 1) == 8){
+                //making sure its not being pushed off map
+                if(playerLoc.get(1) - 1 == 0)return false;
+                if(currGame.getCurrMap().checkTile(playerLoc.get(0),playerLoc.get(1) - 2) == 0 || currGame.getCurrMap().checkTile(playerLoc.get(0),playerLoc.get(1) - 2) == 7){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+         //move right
+        }else if(move == 2){
+            if(playerLoc.get(0) + 1 == currGame.getCurrMap().getMapSize().get(0)) return false;
+            if(currGame.getCurrMap().checkTile(playerLoc.get(0) + 1,playerLoc.get(1)) == 0 || currGame.getCurrMap().checkTile(playerLoc.get(0) + 1 ,playerLoc.get(1)) == 7){
+                return true;
+            }else if(currGame.getCurrMap().checkTile(playerLoc.get(0) + 1,playerLoc.get(1)) == 5 || currGame.getCurrMap().checkTile(playerLoc.get(0) + 1,playerLoc.get(1)) == 8){
+                //making sure its not being pushed off map
+                if(playerLoc.get(0) + 2 == currGame.getCurrMap().getMapSize().get(0))return false;
+                if(currGame.getCurrMap().checkTile(playerLoc.get(0) + 2,playerLoc.get(1)) == 0 || currGame.getCurrMap().checkTile(playerLoc.get(0) + 2,playerLoc.get(1) - 2) == 7){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
         }
-        return true;
+        return false;
     }
 
     public void makeMove(int move){
 
     }
 }
+
