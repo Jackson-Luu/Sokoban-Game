ff2a3661a00e78844fc0a9d11b60f0216aecd1ea
JacksonLuu <jacksonluu2006@hotmail.com>
2017-05-20 20:46:23 +1000

    updated for main menu

--- Project/src/Game2017.java
+++ Project/src/Game2017.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Jackson Luu, Keller Huang, Dan Yoo, Evan Han and Lilac Liu
+ *
+ */
+
 public class Game2017 {
+	
 	public static void main(String[] args){
-		new Sokoban(new GameEngine(15, 15));	
+		new MainMenu();
+		//new Sokoban(new GameEngine(15, 15));
 	}
 }	
-
